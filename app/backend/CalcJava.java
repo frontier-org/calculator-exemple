376acd157b1f8120800dbb2027e25685da9c95ff
Kaio HSG <kaio12hsg@gmail.com>
2026-01-20 21:44:30 -0300

    Case file name update

--- app/backend/CalcJava.java
+++ app/backend/CalcJava.java
@@ -3,7 +3,7 @@
 
 import java.util.Locale;
 
-public class calc_java {
+public class CalcJava {
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Error: No expression provided");
